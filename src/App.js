c41ba551a9986b0f438635e551154ab265ff9c5e
milenb17 <milenb17@gmail.com>
2022-09-14 21:28:09 -0400

    browser basename add

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
     
   }
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/shopping-cart/'>
       <Nav count={numCartItems}/>
       <Routes>
         <Route path='/' element={<Home />} />
